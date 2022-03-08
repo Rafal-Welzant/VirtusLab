179eeccac9986275e3d658f1ad6719bbea787e36
Rafal Welzant <welzant@gmail.com>
2022-03-08 20:13:49 +0100

    poke-app-done

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,20 +1,16 @@
-import React, { Fragment, useEffect, useState } from "react";
-import PokemonThumb from "./components/PokemonThumb";
+import React, { useEffect, useState } from "react";
+import { PokemonCard } from "./components/PokemonCard";
 
-type Pokemon = {
+export type Pokemon = {
   id: number;
-  sprites: { other: { dream_world: { front_default: string } } }
+  sprites: { other: { dream_world: { front_default: string } } };
   name: string;
-  types: Array<{ type: { name: string } } >
+  types: Array<{ type: { name: string } }>;
   weight: number;
   height: number;
-}
+};
 
 const App = () => {
-  const [selectedPokemonId, setSelectedPokemonId] = useState<null | number>(null);
-  const toggleDetailsId = (id: number) => {
-    setSelectedPokemonId((oldId) => (oldId === id ? null : id));
-  };
   const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]);
   const [loadMore, setLoadMore] = useState(
     "https://pokeapi.co/api/v2/pokemon?limit=20"
@@ -22,22 +18,11 @@ const App = () => {
 
   const getAllPokemons = async () => {
     const res = await fetch(loadMore);
-    const data: { next: string; results: Pokemon[]} = await res.json();
+    const data: { next: string; results: Pokemon[] } = await res.json();
 
     setLoadMore(data.next);
 
-    function createPokemonObject(results: Pokemon[]) {
-      results.forEach(async (pokemon) => {
-        const res = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-        );
-        const data = await res.json();
-        setAllPokemons((currentList) => [...currentList, data]);
-        await allPokemons.sort((a, b) => a.id - b.id);
-      });
-    }
-    createPokemonObject(data.results);
-    // console.log(data);
+    setAllPokemons(old => [...old, ...data.results])
   };
   const changeTheme = () => {
     let element = document.body;
@@ -58,44 +43,8 @@ const App = () => {
       <div className="pokemon-container">
         <div className="all-container">
           {allPokemons.map((pokemonStats, index) => {
-            const isExpanded = selectedPokemonId === pokemonStats.id;
-            return (
-              <Fragment key={index}>
-                <PokemonThumb
-                  id={pokemonStats.id}
-                  image={pokemonStats.sprites.other.dream_world.front_default}
-                  name={pokemonStats.name}
-                  type={pokemonStats.types[0].type.name}
-                  isExpanded={isExpanded}
-                  toggleDetailsId={toggleDetailsId}
-                />
-
-                {isExpanded && (
-                  <div
-                    className="details"
-                    onClick={(event) => {
-                      if (event.target === event.currentTarget) {
-                        setSelectedPokemonId(null);
-                      }
-                    }}
-                  >
-                    <dialog open className="dialog">
-                      <img
-                        className="img"
-                        src={
-                          pokemonStats.sprites.other.dream_world.front_default
-                        }
-                      ></img>
-                      <br></br>
-                      Weight: {pokemonStats.weight}
-                      <hr></hr>
-                      Height: {pokemonStats.height}
-                      <br></br>
-                    </dialog>
-                  </div>
-                )}
-              </Fragment>
-            );
+            
+            return <PokemonCard pokemonName={pokemonStats.name} />
           })}
         </div>
         <button className="load-more" onClick={() => getAllPokemons()}>
---
+++ pokedex/src/components/PokemonCard.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import PokemonThumb from "./PokemonThumb";
+import type { Pokemon } from "../App";
+
+type Props = {
+  pokemonName: string;
+};
+
+const loadPokemon = async (name: string): Promise<Pokemon> => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  return await res.json();
+};
+
+export const PokemonCard = ({ pokemonName }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+
+  useEffect(() => {
+    loadPokemon(pokemonName).then(setPokemon);
+  }, [pokemonName]);
+
+  if (pokemon === null) {
+    return <>Loading....</>;
+  }
+
+  return (
+    <>
+      <PokemonThumb
+        id={pokemon.id}
+        image={pokemon.sprites.other.dream_world.front_default}
+        name={pokemon.name}
+        type={pokemon.types[0].type.name}
+        isExpanded={isExpanded}
+        toggleDetailsId={() => setIsExpanded(true)}
+      />
+
+      {isExpanded && (
+        <div
+          className="details"
+          onClick={(event) => {
+            if (event.target === event.currentTarget) {
+              setIsExpanded(false);
+            }
+          }}
+        >
+          <dialog open className="dialog">
+            <img
+              className="img"
+              src={pokemon.sprites.other.dream_world.front_default}
+            ></img>
+            <br></br>
+            Weight: {pokemon.weight}
+            <hr></hr>
+            Height: {pokemon.height}
+            <br></br>
+          </dialog>
+        </div>
+      )}
+    </>
+  );
+};
